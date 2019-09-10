60227035ee1f921218bf4d09121b2e05b13fab27
seiya-mogami <seiya.mogami@dena.com>
2019-09-10 18:09:00 +0900

    Initial Commit

---
+++ MovieTicketPriceModeling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieTicketPriceModeling
+//
+//  Created by Seiya Mogami on 2019/09/10.
+//  Copyright © 2019 Seiya Mogami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
